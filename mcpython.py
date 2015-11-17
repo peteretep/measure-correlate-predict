b9307114503d8081698a7a18b7be173456ab4d21
Peter Armstrong <peteretep@gmail.com>
2015-11-17 22:20:11 +0000

    answers are ok

--- mcpython.py
+++ mcpython.py
@@ -1,13 +1,11 @@
-import csv
-from datetime import datetime
-from itertools import islice
 from scipy.stats import linregress
-import matplotlib.pyplot as plt
 from IPython import embed
 import pandas as pd
+import numpy as np
 
 
 class memoize(dict):
+    """Used to memoize function values"""
     def __init__(self, func):
         self.func = func
 
@@ -20,35 +18,38 @@ class memoize(dict):
 
 
 def kts_to_ms(kts_spd):
+    """Converts a wind speed in knots to m/s"""
     return kts_spd * 0.514
 
 
 def extrapolate_to_60(ms_speed):
-    spd_60 = ms_speed * (60 / 10) ** 0.143
+    """Extrapolates a wind speed to a height of 60m using
+       a shear coefficient 0.2"""
+    spd_60 = ms_speed * (60 / 10) ** 0.2
     return spd_60
 
 
 @memoize
 def raw_mast_data():
+    """Reads the mast_data.csv file and returns a dataframe
+       with mast wind speed at 60m.
+       It takes the maximum value from the two anemometers"""
     df = pd.read_csv('mast_data.csv',
                      header=11,
                      parse_dates=['time'],
+                     dayfirst=True,
                      usecols=[0, 1, 2, 8],
                      names=['time', 's60_one', 's60_two', 'mast_dir'],
                      index_col='time')
-    df['mean_mast_speed'] = pd.Series(((df.s60_one + df.s60_two) / 2),
-                                      index=df.index)
+    df['mean_mast_speed'] = df[['s60_one', 's60_two']].max(axis=1)
     return df
 
 
 @memoize
 def cleaned_mast_data():
+    """Removes any data containing all 0's - when the mast is broken"""
+    # TODO: Remove any data when the temperature is below zero!?
     df = raw_mast_data()[(raw_mast_data()['s60_one'] != 0) & (raw_mast_data()['s60_two'] != 0) & (raw_mast_data()['mast_dir'] != 0)]
-    df = drop_unused_cols(df)
-    return df
-
-
-def drop_unused_cols(df):
     df = df.drop('s60_one', 1)
     df = df.drop('s60_two', 1)
     return df
@@ -58,6 +59,7 @@ def hourly_mast_data():
     return cleaned_mast_data().groupby(pd.TimeGrouper(freq='H')).mean()
 
 
+@memoize
 def raw_reference_data():
     df = pd.read_csv('reference_site_wind_speed_dir_1980_2013.csv',
                      parse_dates={'time': [0, 1, 2, 3]},
@@ -70,6 +72,10 @@ def raw_reference_data():
 
 
 def extrapolated_reference_data():
+    """Returns a dataframe with the hourly wind speeds:
+        - measured at the reference site,
+        - extrapolated to 60m
+    """
     df = raw_reference_data()
     df['extr_speed_ms'] = pd.Series(extrapolate_to_60(df['ref_speed_ms']))
     return df
@@ -77,53 +83,80 @@ def extrapolated_reference_data():
 
 @memoize
 def joined_2012_data():
-    df = hourly_mast_data().join(extrapolated_reference_data()).dropna()
+    """Returns a dataframe with the data for 2012"""
+    df = hourly_mast_data().join(extrapolated_reference_data(), how='inner').dropna()
     return df
 
 
-# def plot_speeds_with_time():
-#     plt.plot(mast_times(), mast_speeds(), 'ro')
-#     plt.plot(reference_times(), reference_speeds(), 'bo')
-#     plt.ylabel('Wind Speed')
-#     plt.xlabel('Time')
-#     plt.show()
+def predicted_speeds():
+    """Returns a dataframe with the hourly wind speeds:
+        - measured at the reference site,
+        - extrapolated to 60m
+        - predicted at the mast site
+       """
+    slope, intercept, r_value, p_value, std_err = linregress(joined_2012_data().mean_mast_speed.tolist(), joined_2012_data().extr_speed_ms.tolist())
+    df = extrapolated_reference_data()
+    df['site_speed'] = pd.Series((df['extr_speed_ms'] - intercept) / slope)
+    df = df.drop('wdir_deg', 1)
+    return df
 
 
-# def plot_ref_mast():
-#     plt.plot(reference_speeds(), mast_speeds(), 'ro')
-#     plt.ylabel('Mast Speeds')
-#     plt.xlabel('Reference Speeds')
-#     plt.show()
+def mean_speeds_from_predicted():
+    """Returns a dataframe with yearly mean windspeeds for:
+        - measured at the reference site,
+        - extrapolated to 60m
+        - predicted at the mast site
+    """
+    return predicted_speeds().groupby(predicted_speeds().index.year).mean()
 
 
-# correlation_coeff = pearsonr(mast_speeds(), reference_speeds())
-# print "The Pearson correlation coefficient is %f" % correlation_coeff[0]
+def mean_speed_from_mast():
+    """Returns the mean wind speed measured at the mast site"""
+    return np.mean(cleaned_mast_data().mean_mast_speed.as_matrix())
+
+
+def rms_error():
+    """Returns the Root Mean Square Error between
+       predicted and measured speeds"""
+    df = predicted_speeds().join(hourly_mast_data()).dropna()
+    actual = df.mean_mast_speed.as_matrix()
+    predicted = df.site_speed.as_matrix()
+    rmse = np.sqrt(((actual - predicted) ** 2).mean())
+    return rmse
+
+
+def bias_error_degrees():
+    """The problem is getting the average windspeed for the hour from the mast.
+       This is where wrap around is an issue.
+       The hourly data we have collected in joined_2012_data may contain
+       incorrect means wind directions per hour"""
+    df = joined_2012_data()
+    mast_direction = df.mast_dir.as_matrix()
+    site_direction = df.wdir_deg.as_matrix()
+    bias = (mast_direction - site_direction).mean()
+    return bias
+
 
 slope, intercept, r_value, p_value, std_err = linregress(joined_2012_data().mean_mast_speed.tolist(), joined_2012_data().extr_speed_ms.tolist())
 
-print "Pearson correlation coefficient: %f" % r_value
-print "Slope: %f" % slope
-print "Intercept: %f" % intercept
+print "Pearson correlation coefficient:"
+print r_value
+print "Slope:"
+print slope
+print "Intercept:"
+print intercept
 
+print "Predicted mean wind speeds per year:"
+print mean_speeds_from_predicted()
 
-# def predict(ref_speed):
-#     site_speed = (slope * ref_speed) + intercept
-#     return site_speed
+print "Measured mean wind speed from mast site:"
+print mean_speed_from_mast()
 
+print "The Root Mean Square Error between predicted and measured windspeed:"
+print rms_error()
 
-# def predicted_speeds():
-#     predicted_data = []
-#     for row in reference_data():
-#         row_dict = {'time': row['time'],
-#                     'reference_speed': row['speed'],
-#                     'site_speed': predict(row['speed'])}
-#         predicted_data.append(row_dict)
-#     return predicted_data
-
-
-embed()
-# cProfile.run(pearsonr(mast_speeds(), reference_speeds()))
+print "The Bias error in degrees:"
+print bias_error_degrees()
 # embed()
 
-# TODO: Get average of mast measurements
-# TODO: Save big_dict to a new csv file
+# TODO: Get average wind direction of mast measurements
